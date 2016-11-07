ae2f76aaadda95c12610421f3f7ce28e70cb32e2
ACER-PC <ACER-PC@ACER>
2016-11-07 16:50:05 -0600

    version 3

---
+++ src/juegoidioma/Respuesta.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegoidioma;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Respuesta {
+    
+private String respuesta;
+private String duda1;
+private String duda2;
+private String duda3;
+
+    public Respuesta(String respuesta, String duda1, String duda2, String duda3) {
+        this.respuesta = respuesta;
+        this.duda1 = duda1;
+        this.duda2 = duda2;
+        this.duda3 = duda3;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public String getDuda1() {
+        return duda1;
+    }
+
+    public void setDuda1(String duda1) {
+        this.duda1 = duda1;
+    }
+
+    public String getDuda2() {
+        return duda2;
+    }
+
+    public void setDuda2(String duda2) {
+        this.duda2 = duda2;
+    }
+
+    public String getDuda3() {
+        return duda3;
+    }
+
+    public void setDuda3(String duda3) {
+        this.duda3 = duda3;
+    }
+    
+    
+
+}
